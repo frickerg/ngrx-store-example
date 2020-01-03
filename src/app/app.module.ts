0a8cb3bccb76bb9b23588ccd9c8f35a0bdae3f6c
Guillaume Fricker <fricker.g@zoho.com>
2020-01-03 11:43:49 +0100

    Updated app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { counterReducer } from './counter.reducer';
 import { CounterComponent } from './components/counter/counter.component';
 import { LoginComponent } from './components/login/login.component';
-import * as fromScoreboard from './reducers/scoreboard.reducer';
+import { MoviesComponent } from './components/movies/movies.component';
 import { ScoreboardComponent } from './components/scoreboard/scoreboard.component';
+import { counterReducer } from './counter.reducer';
+import { MovieEffects } from './effects/movie.effects';
+import * as fromScoreboard from './reducers/scoreboard.reducer';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		CounterComponent,
 		LoginComponent,
-		ScoreboardComponent
+		ScoreboardComponent,
+		MoviesComponent
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
-		StoreModule.forFeature(fromScoreboard.scoreboardFeatureKey, fromScoreboard.reducer),
+		EffectsModule.forRoot([MovieEffects]),
 		StoreModule.forRoot({
 			count: counterReducer,
 			game: fromScoreboard.reducer
